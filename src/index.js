29ac08fc04aa3822d68f51298c883e748ed9ab18
IsmagilovRF <ledohod@icloud.com>
2021-01-15 11:46:19 +0300

    первый коммит hw21

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.css';
+console.log('npm run serve всем привет из файла  index.js. Возможна оперативная отладка кода JS')
+console.log('npm run serve отладка стилей))) styles.css')
+console.log('новая команда: "serve": "./node_modules/.bin/webpack serve",')
+
